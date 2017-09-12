fb17b321cde8a6115e8af09e6972c886b91f303b
AJ Larson <aj.larson@devmounta.in>
2017-09-12 14:19:28 -0600

    problem 4

--- practice.js
+++ practice.js
@@ -6,7 +6,7 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+//Code here
 let me = {
   name: 'AJ',
   age: 29
@@ -23,7 +23,7 @@ window.alert(me.name)
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+//Code here
 
 let favoriteThings = {
   band: 'Imagine Dragons',
@@ -36,7 +36,7 @@ let favoriteThings = {
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+//Code here
 
 favoriteThings.car = 'Spyder'
 
@@ -46,7 +46,7 @@ favoriteThings.brand = 'Apple'
 
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 
-  //Code here
+//Code here
 
 favoriteThings.food = 'Chicken Nuggets'
 
@@ -67,7 +67,7 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. 
 */
 
-  //Code here
+//Code here
 
 
 let backPack = {}
@@ -79,9 +79,9 @@ backPack.color = 'blue';
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+//Code here
 
- window.alert(backPack);
+window.alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -89,7 +89,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-  //Code here
+//Code here
 
 console.log(backPack)
 
@@ -98,17 +98,17 @@ console.log(backPack)
 
 
 var user2 = {
-        name: 'Ty',
-        age: 24,
-        pwHash: 'U+Ldlngx2BYQk',
-        email: 'ty33@gmail.com',
-        birthday: '05/02/1990',
-        username: 'tylermcginnis33'
+  name: 'Ty',
+  age: 24,
+  pwHash: 'U+Ldlngx2BYQk',
+  email: 'ty33@gmail.com',
+  birthday: '05/02/1990',
+  username: 'tylermcginnis33'
 }
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+//Code Here
 
 user2.name = 'Tyler S. McGinnis'
 user2.email = 'tyler.mcginnis@devmounta.in'
@@ -131,10 +131,9 @@ user2.email = 'tyler.mcginnis@devmounta.in'
 
 //Create an empty object called methodCollection.
 
-  //Code Here
+//Code Here
 
-
-
+let methodCollection = {}
 
 /*
 Now add two methods (functions that are properties on objects) to your methodCollection
@@ -142,20 +141,23 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
  which logs 'hello' to the console. 
  */
 
-  //Code Here
+//Code Here
 
+methodCollection.alertHello = function () {
+  window.alert('hello')
+}
 
-
-
+methodCollection.logHello = function () {
+  console.log('hello')
+}
 
 //Now call your alertHello and logHello methods.
 
   //Code Here
 
+methodCollection.alertHello()
 
-
-
-
+methodCollection.logHello()
 
 
 
@@ -172,7 +174,7 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 
 
-  
+
 
 
 //NEXT PROBLEM
